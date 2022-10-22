779cbbec68d80f837294d8d4e7cf49f1b489b3a0
ligadobra <russiancmo@gmail.com>
2022-10-22 14:47:14 +0300

    add dobro page prototype screen

--- src/pages/Dobro/dobro.tsx
+++ src/pages/Dobro/dobro.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { useQuery } from "react-query";
-import { useAlert } from "react-alert";
-import { axios } from "../../services/axios";
+import styles from "./dobro.module.scss";
 
 export const Dobro = () => {
-  const alert = useAlert();
-  const { data, isLoading } = useQuery("get-test", () =>
-    axios.get("test", { params: { id: "123" } })
+  return (
+    <div className={styles.container}>
+      <div className={styles.dobro}>
+        <div className={styles["help-button"]} onClick={() => {}}>
+          <div className={styles["help-button__item"]}>Добро</div>
+        </div>
+        <div className={styles.info}>Как это работает?</div>
+      </div>
+    </div>
   );
-  return <div onClick={() => alert.success("yeeeees")}>Dobro</div>;
 };
