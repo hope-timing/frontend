ebfaf66a2ab159dac373cd302746e46ebb10a3d9
ligadobra <russiancmo@gmail.com>
2022-10-24 23:52:06 +0300

    add menu to base layout

--- src/layouts/Base/base-layout.tsx
+++ src/layouts/Base/base-layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Menu } from "../../components/Menu/menu";
 import styles from "./base-layout.module.scss";
 
 export const BaseLayout = () => {
   return (
     <div className={styles.base}>
+      <Menu />
       <Outlet />
     </div>
   );
