a761746bea111b8d0aac102ca5cba761c9adc729
ligadobra <russiancmo@gmail.com>
2022-10-26 12:28:29 +0300

    add TextField component

--- src/components/TextField/text-field.tsx
+++ src/components/TextField/text-field.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Icon from "../Icon";
 import { ITextField } from "./text-field.types";
+import cn from "classnames";
 import styles from "./text-field.module.scss";
 
-export const TextField = ({ type, title, placeholder }: ITextField) => {
+export const TextField = forwardRef(({
+  type,
+  placeholder,
+  onChange,
+  onBlur,
+  value,
+  name,
+  icon,
+  iconWidth,
+  fullWidth,
+}: ITextField, ref: any) => {
   return (
     <>
-      <Icon type='icon-basket' width={40} />
-      <input type={type} placeholder={placeholder} hidden />
+      <div className={cn(styles.input, fullWidth && styles.fullWidth)}>
+        {icon && (
+          <div className={styles["input__icon"]}>
+            <Icon type={icon} width={iconWidth ?? 20} />
+          </div>
+        )}
+        <input
+          name={name}
+          placeholder={placeholder}
+          value={value ?? ''}
+          type={type}
+          onChange={onChange}
+          onBlur={onBlur}
+          className={styles["input__field"]}
+          ref={ref}
+        />
+      </div>
     </>
   );
-};
+});
--- src/components/TextField/text-field.types.ts
+++ src/components/TextField/text-field.types.ts
@@ -1,5 +1,8 @@
 import { InputHTMLAttributes } from "react";
+import { TIcon } from "../Icon/icon.types";
 
 export interface ITextField extends InputHTMLAttributes<HTMLInputElement> {
-  text?: string;
+  icon?: TIcon;
+  iconWidth?: number;
+  fullWidth?: boolean;
 }
