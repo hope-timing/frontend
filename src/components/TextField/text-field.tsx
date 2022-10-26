50feb7b045936462c28f0e591fc218652ef36d67
ligadobra <russiancmo@gmail.com>
2022-10-26 23:31:23 +0300

    fix texField component

--- src/components/TextField/text-field.tsx
+++ src/components/TextField/text-field.tsx
@@ -4,36 +4,43 @@ import { ITextField } from "./text-field.types";
 import cn from "classnames";
 import styles from "./text-field.module.scss";
 
-export const TextField = forwardRef(({
-  type,
-  placeholder,
-  onChange,
-  onBlur,
-  value,
-  name,
-  icon,
-  iconWidth,
-  fullWidth,
-}: ITextField, ref: any) => {
-  return (
-    <>
-      <div className={cn(styles.input, fullWidth && styles.fullWidth)}>
-        {icon && (
-          <div className={styles["input__icon"]}>
-            <Icon type={icon} width={iconWidth ?? 20} />
-          </div>
-        )}
-        <input
-          name={name}
-          placeholder={placeholder}
-          value={value ?? ''}
-          type={type}
-          onChange={onChange}
-          onBlur={onBlur}
-          className={styles["input__field"]}
-          ref={ref}
-        />
-      </div>
-    </>
-  );
-});
+export const TextField = forwardRef(
+  (
+    {
+      type,
+      placeholder,
+      onChange,
+      onBlur,
+      value,
+      name,
+      disabled,
+      icon,
+      iconWidth,
+      fullWidth,
+    }: ITextField,
+    ref: any
+  ) => {
+    return (
+      <>
+        <div className={cn(styles.input, fullWidth && styles.fullWidth)}>
+          {icon && (
+            <div className={styles["input__icon"]}>
+              <Icon type={icon} width={iconWidth ?? 20} />
+            </div>
+          )}
+          <input
+            name={name}
+            placeholder={placeholder}
+            value={value ?? ""}
+            type={type}
+            onChange={onChange}
+            onBlur={onBlur}
+            className={styles["input__field"]}
+            disabled={disabled}
+            ref={ref}
+          />
+        </div>
+      </>
+    );
+  }
+);
