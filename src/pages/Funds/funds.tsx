f1d74ec70d29c66c04fbddb3889e66b673e939d3
ligadobra <russiancmo@gmail.com>
2022-10-26 23:31:50 +0300

    test select and button components

--- src/pages/Funds/funds.tsx
+++ src/pages/Funds/funds.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
+import { Button } from "../../components/Button/button";
+import { SelectField } from "../../components/SelectField/select-field";
 import { TextField } from "../../components/TextField/text-field";
 
 export const Funds = () => {
   const { control, handleSubmit } = useForm();
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data.test);
+    console.log(data);
   });
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form>
         <Controller
           control={control}
           name="test"
@@ -25,7 +27,24 @@ export const Funds = () => {
             />
           )}
         ></Controller>
-        <button type="submit">Тест!</button>
+        <br></br>
+        <Controller
+          control={control}
+          name="select"
+          render={({ field: { onChange, value } }) => (
+            <SelectField
+              value={value}
+              placeholder="Категория"
+              optionsList={[{ id: "123" }]}
+              getLabel={(option) => option?.id ?? ""}
+              onChange={onChange}
+            />
+          )}
+        ></Controller>
+        <br></br>
+        <Button title="Тест1!" onClick={() => onSubmit()} type="submit">
+          Тест1!
+        </Button>
       </form>
     </div>
   );
