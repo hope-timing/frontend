445897abcdd9dca64f625a3893fcf0b7b1cc09c6
ligadobra <russiancmo@gmail.com>
2022-10-26 12:28:41 +0300

    test TextField component

--- src/pages/Funds/funds.tsx
+++ src/pages/Funds/funds.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { TextField } from '../../components/TextField/text-field'
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { TextField } from "../../components/TextField/text-field";
 
 export const Funds = () => {
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data.test);
+  });
+
   return (
     <div>
-      <TextField type='text' placeholder='upss...' />
+      <form onSubmit={onSubmit}>
+        <Controller
+          control={control}
+          name="test"
+          render={({ field }) => (
+            <TextField
+              {...field}
+              type="text"
+              placeholder="Поиск"
+              icon="icon-search"
+              iconWidth={15}
+            />
+          )}
+        ></Controller>
+        <button type="submit">Тест!</button>
+      </form>
     </div>
-  )
-}
+  );
+};
