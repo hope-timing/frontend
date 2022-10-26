1fc37833178ad2e3815f4e508d645508dbb22323
ligadobra <russiancmo@gmail.com>
2022-10-26 23:31:33 +0300

    add useOnClickOutside hook

---
+++ src/hooks/app/useOnClickOutside.ts
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export function useOnClickOutside(
+  ref: React.RefObject<HTMLDivElement>,
+  action: () => void
+) {
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        action();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
