f83d44ea1a15a96a5e89c96bba2e809743123b3e
ligadobra <russiancmo@gmail.com>
2022-10-28 18:23:48 +0300

    add icon to button component

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -3,26 +3,54 @@ import { IButton } from "./button.types";
 import { WithChildren } from "../../types/app/common";
 import cn from "classnames";
 import styles from "./button.module.scss";
+import Icon from "../Icon";
 
 export const Button = ({
   title,
   color,
   onClick,
   children,
+  icon,
   type,
+  onlyIcon,
+  iconFill,
+  iconWidth,
+  iconHeight,
+  iconStroke,
   className,
-}: IButton & WithChildren) => {
+}: IButton & Partial<WithChildren>) => {
   const handleClick = (e: any) => {
     e.preventDefault();
     onClick?.();
   };
   return (
     <button
-      title={title}
-      onClick={(e) => onClick ? handleClick(e) : {}}
-      className={cn(styles.button, className)}
+      title={title ?? ""}
+      onClick={(e) => (onClick ? handleClick(e) : {})}
+      className={cn(
+        styles.button,
+        onlyIcon && styles["button_onlyicon_true"],
+        className
+      )}
     >
-      {children}
+      {icon && (
+        <div
+          className={
+            onlyIcon
+              ? styles["button__icon_onlyicon_true"]
+              : styles["button__icon"]
+          }
+        >
+          <Icon
+            type={icon}
+            width={iconWidth ?? 16}
+            height={iconHeight}
+            stroke={iconStroke ?? "#ffffff"}
+            fill={onlyIcon ? iconFill : ""}
+          />
+        </div>
+      )}
+      {!onlyIcon && <div className={styles["button__title"]}>{children}</div>}
     </button>
   );
 };
--- src/components/Button/button.types.ts
+++ src/components/Button/button.types.ts
@@ -1,11 +1,17 @@
-import { ButtonHTMLAttributes } from "react";
 import { Color } from "../../types/app/colors";
 import { IStyleProps } from "../../types/app/common";
+import { TIcon } from "../Icon/icon.types";
 
 export interface IButton
   extends IStyleProps,
     React.ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string;
+  title?: string;
   onClick?: () => void;
   color?: Color;
+  icon?: TIcon;
+  iconFill?: string;
+  iconStroke?: string;
+  iconWidth?: number;
+  iconHeight?: number;
+  onlyIcon?: boolean;
 }
