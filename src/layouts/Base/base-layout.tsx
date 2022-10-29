2991766b088e659c1975b154bcef25053a84dfb4
ligadobra <russiancmo@gmail.com>
2022-10-29 14:47:04 +0300

    refactor base layout

--- src/layouts/Base/base-layout.tsx
+++ src/layouts/Base/base-layout.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Menu } from "../../components/Menu/menu";
+import cn from "classnames";
 import styles from "./base-layout.module.scss";
 
 export const BaseLayout = () => {
+  const location = useLocation();
+
   return (
-    <div className={styles.base}>
+    <div
+      className={cn(
+        styles.base,
+        location.pathname === "/" && styles["base__container_overflow_hidden"]
+      )}
+    >
       <Menu />
-      <div className={styles["base__container"]}>
+      <div className={cn(styles["base__container"])}>
         <Outlet />
       </div>
     </div>
