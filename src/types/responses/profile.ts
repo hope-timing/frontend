9d4905fc6a21617c441e8a6408a550ec94bdb2b4
ligadobra <russiancmo@gmail.com>
2022-10-30 17:20:28 +0300

    add profile response type prototype

---
+++ src/types/responses/profile.ts
@@ -0,0 +1,13 @@
+type TSex = "male" | "female";
+
+export type TProfile = {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  phone: string;
+  city: string;
+  age: string;
+  sex: TSex;
+  id: string;
+  avatar: string;
+};
