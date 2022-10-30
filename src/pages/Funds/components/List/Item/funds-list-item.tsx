2a5064d9c27062cdbbb87ed5f68e81c9f695b1c8
ligadobra <russiancmo@gmail.com>
2022-10-30 17:19:38 +0300

    redesign funds page

--- src/pages/Funds/components/List/Item/funds-list-item.tsx
+++ src/pages/Funds/components/List/Item/funds-list-item.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { IFundProps } from "./funds-list-item.types";
 import styles from "./funds-list-item.module.scss";
-import { useHover } from "../../../../../hooks/app/useHover";
+import { Button } from "../../../../../components/Button/button";
 
 export const FundsListItem = ({ fund }: IFundProps) => {
-  const [hoverRef, isHovered] = useHover();
-
   return (
     <div className={styles.fund}>
-      <div
-        className={styles["fund__bg"]}
-        ref={hoverRef ? (hoverRef as React.RefObject<HTMLDivElement>) : null}
-      >
-        {!isHovered && (
-          <>
-            <img
-              src={fund.logo}
-              className={styles["fund__logo"]}
-              alt="Логотип благотворительного фонда"
-            />
-            <div className={styles["fund__title"]}>{fund.name}</div>
-          </>
-        )}
-        {isHovered && (
-          <div className={styles["fund__description"]}>{fund.description}</div>
-        )}
+      <div className={styles["fund__bg"]}>
+        <img
+          src={fund.logo}
+          className={styles["fund__logo"]}
+          alt="Логотип благотворительного фонда"
+        />
+        <div className={styles["fund__title"]}>{fund.description}</div>
+        <div className={styles["fund__button"]}>
+          <a href={fund.links.url} target="_blank">
+            Подробнее{" "}
+          </a>
+        </div>
+        <div className={styles["fund__category"]}>{fund.category}</div>
       </div>
       {/* <div className={styles["fund__icons"]}>
         <Button
