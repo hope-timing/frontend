0978f36dcccd528d637030d775d973f31e3709bc
ligadobra <russiancmo@gmail.com>
2022-10-30 17:19:56 +0300

    add inDevelope to statistic page

--- src/pages/Statistic/statistic.tsx
+++ src/pages/Statistic/statistic.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { InDevelope } from "../../components/DevelopingMode/in-develope";
+import styles from "./statistic.module.scss";
 
 export const Statistic = () => {
-  return <div>Statistic</div>;
+  return (
+    <div className={styles.statistic}>
+      <InDevelope absolute />
+    </div>
+  );
 };
