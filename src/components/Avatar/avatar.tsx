d702048b766695ccca138a3fe0613584a0cb8c64
ligadobra <russiancmo@gmail.com>
2022-10-31 23:36:28 +0300

    add avatar overlay prop

--- src/components/Avatar/avatar.tsx
+++ src/components/Avatar/avatar.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import { IAvatarProps } from "./avatar.types";
 import DefaultAvatar from "../../assets/svg/default-avatar.svg";
 import styles from "./avatar.module.scss";
+import { WithRef } from "../../types/app/common";
 
-export const Avatar = ({ src }: IAvatarProps) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles["container__avatar"]}>
-        <img
-          src={src || DefaultAvatar}
-          alt="Аватар пользователя"
-          className={styles.img}
-        />
+export const Avatar = forwardRef(
+  ({ src, isOverlay }: IAvatarProps, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+      <div className={styles.container} ref={ref}>
+        <div className={styles["container__avatar"]}>
+          <img
+            src={src || DefaultAvatar}
+            alt="Аватар пользователя"
+            className={styles.img}
+          />
+          {isOverlay && (
+            <div className={styles["container__avatar_overlay_true"]} />
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
--- src/components/Avatar/avatar.types.ts
+++ src/components/Avatar/avatar.types.ts
@@ -1,3 +1,4 @@
 export interface IAvatarProps {
   src: string;
+  isOverlay?: boolean;
 }
