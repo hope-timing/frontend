ca7c3211b43f4e4c336ef74521fb00e47f021bad
ligadobra <russiancmo@gmail.com>
2022-11-01 23:26:11 +0300

    add dropdown to menu balance

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { menuLinks } from "../../constants/components/Menu/links";
 import { IMenu } from "./menu.types";
 import cn from "classnames";
 import styles from "./menu.module.scss";
 import Icon from "../Icon";
+import { IDropdownList } from "../Dropdown/dropdown.types";
+import { Dropdown } from "../Dropdown/dropdown";
+import { useOnClickOutside } from "../../hooks/app/useOnClickOutside";
 
 export const Menu = ({ withRightSide = false }: IMenu) => {
+  const [isOpenBalanceMenu, setIsOpenBalanceOpenMenu] =
+    useState<boolean>(false);
+  const balanceRef = useRef<HTMLDivElement>(null);
+  const balanceWrapperRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const currentUserBalance = 20;
   const user = true;
+
+  const balanceMenu: IDropdownList[] = [
+    {
+      label: "Обменять баллы",
+      icon: "icon-cart-add",
+      iconFill: "#3c3c3c"
+    },
+    {
+      label: "Пригласить друга",
+      icon: "icon-person-add",
+      iconFill: "#3c3c3c"
+    },
+  ];
+
+  useOnClickOutside(balanceWrapperRef, () => setIsOpenBalanceOpenMenu(false));
 
   return (
     <div className={styles.container}>
@@ -30,11 +52,28 @@ export const Menu = ({ withRightSide = false }: IMenu) => {
         })}
       </div>
       {withRightSide && user && (
-        <div className={styles.balance}>
-          <Icon type="icon-empty-circle" width={20} height={20} stroke="#bfda53" />
-          <div className={styles["balance__title"]}>
-            {`${currentUserBalance ?? 0} DP`}
+        <div className={styles["balance-wrapper"]} ref={balanceWrapperRef}>
+          <div
+            className={styles.balance}
+            onClick={() => setIsOpenBalanceOpenMenu(!isOpenBalanceMenu)}
+            ref={balanceRef}
+          >
+            <Icon
+              type="icon-empty-circle"
+              width={20}
+              height={20}
+              stroke="#bfda53"
+            />
+            <div className={styles["balance__title"]}>
+              {`${currentUserBalance ?? 0} DP`}
+            </div>
           </div>
+          <Dropdown
+            list={balanceMenu}
+            isOpen={isOpenBalanceMenu}
+            close={setIsOpenBalanceOpenMenu}
+            ref={balanceRef}
+          />
         </div>
       )}
     </div>
